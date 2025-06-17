be95c058f177a4314f2e89634a28966eafa47e02
Philip Walton <phil@softforge.co.uk>
2025-06-17 13:11:19 +0000

    Initial commit - which-police-force
    - Added GPL v2 LICENSE
    - Created project structure
    - Added README and documentation
    - Set up softforge profile configuration

---
+++ README.md
@@ -0,0 +1,27 @@
+# which-police-force
+
+## Description
+
+[Add your project description here]
+
+## Installation
+
+```bash
+# Add installation instructions
+```
+
+## Usage
+
+[Add usage instructions]
+
+## License
+
+This project is licensed under the GNU General Public License version 2 or later.
+See the [LICENSE](LICENSE) file for details.
+
+## Author
+
+Philip Walton (phil@softforge.co.uk)
+
+---
+Copyright (C) 2025 SoftForge. All rights reserved.
---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     which-police-force
+ * @subpackage  
+ *
+ * @copyright   Copyright (C) 2025 SoftForge. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Main class for which-police-force
+ *
+ * @since  1.0
+ */
+class Which_police_force
+{
+    /**
+     * Constructor
+     *
+     * @since   1.0
+     */
+    public function __construct()
+    {
+        // Initialize
+    }
+}
